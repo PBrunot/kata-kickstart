28aad76a0da9853d72fdc502d08ce3e2fb439849
Pascal Brunot <pascal.brunot@gmail.com>
2025-09-05 19:07:01 +0200

    bowling kata

--- python/python3-kickstart/python3_kickstart/greetings_package.py
+++ python/python3-kickstart/python3_kickstart/greetings_package.py
@@ -1,5 +1,33 @@
-def hello():
-    return "Hello!"
 
-def hey():
-    return "Hey!"
+def calcola_punteggio(tiri: list[int]) -> int:
+    punteggio = 0
+    num_frame = 1
+    num_tiro = 0
+    tot_frame = 0
+
+    for idx in range(0, len(tiri)):
+        tiro = tiri[idx]
+        print(f"Frame: {num_frame}, Tiro: {num_tiro}, Birilli: {tiro},Punteggio: {punteggio}")
+        if num_frame > 10:
+            break
+
+        if tiro == 10:
+            punteggio  += 10
+            num_frame += 1
+            num_tiro = 1
+            tot_frame = 0
+            print("STRIKE!")
+            punteggio+= tiri[idx+1] + tiri[idx+2]
+            continue
+        punteggio += tiro
+        tot_frame += tiro
+        num_tiro+=1
+        if num_tiro == 2:
+            num_tiro = 0
+            num_frame += 1
+            tot_frame = 0
+        if tot_frame == 10:
+            print("SPARE!")
+            punteggio += tiri[idx+1]
+        
+    return punteggio
--- python/python3-kickstart/tests/test_greetings_package.py
+++ python/python3-kickstart/tests/test_greetings_package.py
@@ -2,6 +2,22 @@ from unittest import TestCase
 from python3_kickstart import greetings_package
 
 class Test_Greetings_Package(TestCase):
-    def test_hello(self):
-        greeting = greetings_package.hello()
-        self.assertEqual(greeting, "helooo")
+    def test_zero(self):
+        self.assertEqual(greetings_package.calcola_punteggio([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]), 0)
+    
+    def test_all_ones(self):
+        self.assertEqual(greetings_package.calcola_punteggio([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]), 20)
+    
+    def test_spare(self):
+        self.assertEqual(greetings_package.calcola_punteggio([5,5,3]), 13)
+    
+    def test_strike(self):
+        self.assertEqual(greetings_package.calcola_punteggio([10,3,4]), 24)
+    
+    def test_strike_spare(self):
+        self.assertEqual(greetings_package.calcola_punteggio([10,5,5,0,0,0,0]), 30)
+
+    def test_perfect_game(self):
+        self.assertEqual(greetings_package.calcola_punteggio([10,10,10,10,10,10,10,10,10,10,10,10]), 300)
+    
+    
